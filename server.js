f46907ad84bbe67c7b9a82f3f06bdf0e7b63883d
Ryan Taylor <ryan.taylor1@yahoo.com>
2022-12-17 14:55:07 -0800

    removed config, fixed dotenv bug, added all roles function

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const mysql = require('mysql2');
-const inquirer = require('inquirer');
-const fs = require('fs');
-
+const express = require("express");
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+const fs = require("fs");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,10 +12,10 @@ app.use(express.json());
 
 const db = mysql.createConnection(
   {
-    host: 'localhost',
+    host: "localhost",
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
   },
   console.log(`Connected to the employee_db database.`)
 );
@@ -24,44 +24,54 @@ const promptUser = async () => {
   let data = await inquirer.prompt(promptQuestions);
 
   switch (data.action) {
-    case 'View all departments':
+    case "View all departments":
       allDepartments();
       break;
-
-
-}};
+    case "View all roles":
+      allRoles();
+      break;
+  }
+};
 
 const promptQuestions = [
   {
-    type: 'list',
-    name: 'action',
-    message: 'What would you like to do?',
+    type: "list",
+    name: "action",
+    message: "What would you like to do?",
     choices: [
-      'View all employees',
-      'View all employees by department',
-      'View all employees by manager',
-      'Add employee',
-      'Remove employee',
-      'Update employee role',
-      'Update employee manager',
-      'View all roles',
-      'Add role',
-      'Remove role',
-      'View all departments',
-      'Add department',
-      'Remove department',
-      'Quit'
-    ]
-  }
+      "View all employees",
+      "View all employees by department",
+      "View all employees by manager",
+      "Add employee",
+      "Remove employee",
+      "Update employee role",
+      "Update employee manager",
+      "View all roles",
+      "Add role",
+      "Remove role",
+      "View all departments",
+      "Add department",
+      "Remove department",
+      "Quit",
+    ],
+  },
 ];
 
 const allDepartments = () => {
-  db.query('SELECT * FROM department', function (err, results) {
+  db.query("SELECT * FROM department", function (err, results) {
     console.table(results);
     promptUser();
-  })};
+  });
+};
 
-promptUser()
+const allRoles = () => {
+  db.query("SELECT * FROM role", function (err, results) {
+    console.table(results);
+    promptUser();
+  });
+};
+
+promptUser();
 
 app.use((req, res) => {
   res.status(404).end();
